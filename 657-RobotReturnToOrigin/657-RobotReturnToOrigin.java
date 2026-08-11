25fbacd37b101788cad05cbef7fb2aafcef242c9
Ahelan V <ahelanv2006@gmail.com>
2026-08-11 16:02:18 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,26 @@
+// Last updated: 8/11/2026, 4:02:17 PM
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+
+        for (char move : moves.toCharArray()) {
+            switch (move) {
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
