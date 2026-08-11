60e47c48a6ac57537ab1ecfb96d3f5c928d08e62
Ahelan V <ahelanv2006@gmail.com>
2026-08-11 15:59:54 +0530

    Create 2025-RedistributeCharactersToMakeAllStringsEqual

---
+++ 2025-RedistributeCharactersToMakeAllStringsEqual/2025-RedistributeCharactersToMakeAllStringsEqual.java
@@ -0,0 +1,23 @@
+// Last updated: 8/11/2026, 3:59:53 PM
+class Solution {
+    public boolean makeEqual(String[] words) {
+        int[] fre = new int[26];
+
+        for(int i=0; i<words.length; i++){
+            String w = words[i];
+
+            for(int j=0; j<w.length(); j++){
+                char ch = w.charAt(j);
+                int idx = (int)(ch-'a');
+
+                fre[idx] = fre[idx] + 1;
+            }
+        }
+        for(int i=0; i<26; i++){
+            if(fre[i] % words.length != 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
