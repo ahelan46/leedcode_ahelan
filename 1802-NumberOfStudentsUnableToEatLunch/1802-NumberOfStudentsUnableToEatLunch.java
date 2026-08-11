b2ae5858fbb96301186f52fbbeef70844c4f7b85
Ahelan V <ahelanv2006@gmail.com>
2026-08-11 16:00:25 +0530

    Create 1802-NumberOfStudentsUnableToEatLunch

---
+++ 1802-NumberOfStudentsUnableToEatLunch/1802-NumberOfStudentsUnableToEatLunch.java
@@ -0,0 +1,29 @@
+// Last updated: 8/11/2026, 4:00:24 PM
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        int ones = 0; //count of students who prefer type1
+        int zeros = 0; //count of students who prefer type0
+		
+        for(int stud : students){
+            if(stud == 0) zeros++;
+            else ones++;
+        }
+        
+        // for each sandwich in sandwiches
+        for(int sandwich : sandwiches){
+            if(sandwich == 0){  // if sandwich is of type0
+                if(zeros == 0){ // if no student want a type0 sandwich
+                    return ones;
+                }
+                zeros--;
+            }
+            else{  // if sandwich is of type1
+                if(ones == 0){  // if no student want a type1 sandwich 
+                    return zeros;
+                }
+                ones--;
+            }
+        }
+        return 0;
+    }
+}
