94919a07c9a382e8ae605c796e3c903599d6b4c9
Ahelan V <ahelanv2006@gmail.com>
2026-08-11 16:05:34 +0530

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,10 @@
+-- Last updated: 8/11/2026, 4:05:33 PM
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT l1.num AS "ConsecutiveNums"
+FROM Logs l1
+JOIN Logs l2
+    ON l1.id = l2.id - 1
+JOIN Logs l3
+    ON l2.id = l3.id - 1
+WHERE l1.num = l2.num
+  AND l2.num = l3.num;
