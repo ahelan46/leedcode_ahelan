932d0bb574713901f2ebb1073718e3f45dfe9b37
Ahelan V <ahelanv2006@gmail.com>
2026-08-11 15:59:32 +0530

    Create 2219-MaximumNumberOfWordsFoundInSentences

---
+++ 2219-MaximumNumberOfWordsFoundInSentences/2219-MaximumNumberOfWordsFoundInSentences.java
@@ -0,0 +1,18 @@
+// Last updated: 8/11/2026, 3:59:31 PM
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max=0;
+        for(int i=0;i<sentences.length;i++){
+            int c=1;
+            for(int j=0;j<sentences[i].length();j++){
+                if(sentences[i].charAt(j)==' ')
+                    c++;
+                
+            }
+            if(c>max)
+            max=c;
+
+        }
+        return max;
+    }
+}
