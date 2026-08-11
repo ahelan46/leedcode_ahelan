f8dce65c2ae3619209e13de55211ecef8728cf3c
Ahelan V <ahelanv2006@gmail.com>
2026-08-11 16:04:00 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,11 @@
+// Last updated: 8/11/2026, 4:03:59 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0)
+        return false;
+        else if(n==n)
+        return true;
+        else
+        return false;
+    }
+}
