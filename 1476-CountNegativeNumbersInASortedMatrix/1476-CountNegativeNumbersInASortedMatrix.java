7e3308b47d7a8288a213c6113187dca8dcfabdaa
Ahelan V <ahelanv2006@gmail.com>
2026-08-11 16:01:02 +0530

    Create 1476-CountNegativeNumbersInASortedMatrix

---
+++ 1476-CountNegativeNumbersInASortedMatrix/1476-CountNegativeNumbersInASortedMatrix.java
@@ -0,0 +1,13 @@
+// Last updated: 8/11/2026, 4:01:01 PM
+class Solution {
+    public int countNegatives(int[][] grid) {
+        int c=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[i].length;j++){
+            if(grid[i][j]<0)
+             c++;
+        }
+        }
+        return c;
+    }
+}
