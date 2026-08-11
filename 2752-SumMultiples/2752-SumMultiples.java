6332d457caa5b2a19a2bc3179ba0821e39a217a5
Ahelan V <ahelanv2006@gmail.com>
2026-08-11 15:58:49 +0530

    Create 2752-SumMultiples

---
+++ 2752-SumMultiples/2752-SumMultiples.java
@@ -0,0 +1,10 @@
+// Last updated: 8/11/2026, 3:58:48 PM
+class Solution {
+    public int sumOfMultiples(int n) {
+        return divisum(n, 3) + divisum(n, 5) + divisum(n, 7) + divisum(n, 105) - divisum(n, 15) - divisum(n, 21) - divisum(n, 35);
+    }
+    private int divisum(int n , int k){
+        int m = n / k;
+        return k * m * (m + 1) / 2;
+    }
+}
